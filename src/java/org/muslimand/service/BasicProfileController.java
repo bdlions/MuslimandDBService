4e2167e2f58a967a89356bfaa099dfd0195e89fb
Rashida <rashidacse@gmail.com>
2015-06-27 08:57:23 +0600

    basicProfile

--- src/java/org/muslimand/service/BasicProfileController.java
+++ src/java/org/muslimand/service/BasicProfileController.java
@@ -5,6 +5,7 @@
  */
 package org.muslimand.service;
 
+import com.shampan.db.services.BasicProfileService;
 import org.json.JSONObject;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,6 +21,38 @@ public class BasicProfileController {
      @RequestMapping("/getBasicInfo")
     public String getBasicInfo(@RequestParam(value = "name", defaultValue = "Alamgir") String name) {
         return name;
+    }
+    
+    @RequestMapping("/getWorksEducation")
+    public String getWorksEducation(String userId) {
+        return BasicProfileService.getWorksEducation(userId);
+    }
+    @RequestMapping("/addWorkPlace")
+    public String addWorkPlace( @RequestParam String userId , @RequestParam String additionalData) {
+            String response =  BasicProfileService.addWorkPlace(userId,additionalData);
+        return response;
+    }
+    
+    @RequestMapping("/addPSkill")
+    public String addPSkill( @RequestParam String userId , @RequestParam String additionalData) {
+            String response =  BasicProfileService.addPSkill(userId,additionalData);
+        return response;
+    }
+    @RequestMapping("/addUniversity")
+    public String addUniversity( @RequestParam String userId , @RequestParam String additionalData) {
+            String response =  BasicProfileService.addUniversity(userId,additionalData);
+        return response;
+    }
+    @RequestMapping("/addCollege")
+    public String addCollege( @RequestParam String userId , @RequestParam String additionalData) {
+            String response =  BasicProfileService.addCollege(userId,additionalData);
+        return response;
+    }
+    
+    @RequestMapping("/addSchool")
+    public String addSchool( @RequestParam String userId , @RequestParam String additionalData) {
+            String response =  BasicProfileService.addSchool(userId,additionalData);
+        return response;
     }
     
     
