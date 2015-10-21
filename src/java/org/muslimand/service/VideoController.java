20ca25ec73a376629974394b72ff84970728d73a
rashidacse <rashida57pust@gmail.com>
2015-10-21 18:07:23 +0600

    video module methods

--- src/java/org/muslimand/service/VideoController.java
+++ src/java/org/muslimand/service/VideoController.java
@@ -17,9 +17,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/video")
 public class VideoController {
 
-    @RequestMapping("/getCategories")
-    public String getCategories() {
-        return VideoService.getCategories();
+    @RequestMapping("/getVideoCategories")
+    public String getVideoCategories() {
+        return VideoService.getVideoCategories();
     }
 
     @RequestMapping("/addVideo")
@@ -32,8 +32,36 @@ public class VideoController {
         return VideoService.getVideo(videoId);
     }
 
+    @RequestMapping("/updateVideo")
+    public static String updateVideo(String videoId,String videoInfo) {
+        return VideoService.updateVideo(videoId,videoInfo);
+    }
     @RequestMapping("/deleteVideo")
     public static String deleteVideo(String videoId) {
         return VideoService.deleteVideo(videoId);
+    }
+    @RequestMapping("/addVideoLike")
+    public static String addVideoLike(String videoId,String likeInfo) {
+        return VideoService.addVideoLike(videoId,likeInfo);
+    }
+      @RequestMapping("/getVideoLikeList")
+    public static String getVideoLikeList(String videoId) {
+        return VideoService.getVideoLikeList(videoId);
+    }
+      @RequestMapping("/deleteVideoLike")
+    public static String deleteVideoLike(String videoId,String likeId) {
+        return VideoService.deleteVideoLike(videoId,likeId);
+    }
+      @RequestMapping("/addVideoComment")
+    public static String addVideoComment(String videoId,String commentInfo) {
+        return VideoService.addVideoComment(videoId,commentInfo);
+    }
+      @RequestMapping("/getVideoComments")
+    public static String getVideoComments(String videoId) {
+        return VideoService.getVideoComments(videoId);
+    }
+      @RequestMapping("/editVideoComment")
+    public static String editVideoComment(String videoId,String commentId, String commentInfo) {
+        return VideoService.editVideoComment(videoId, commentId,commentInfo);
     }
 }
