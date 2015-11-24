ab4c6aa6f5f6d0164c414a0be2e24739dd426fe5
rashidacse <rashida57pust@gmail.com>
2015-11-24 20:15:38 +0600

    user default profile  add

--- src/java/org/muslimand/service/RelationController.java
+++ src/java/org/muslimand/service/RelationController.java
@@ -11,57 +11,54 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/relation")
 public class RelationController {
+
     @RequestMapping("/addFriend")
-    public String addFriend(String fromUserId, String toUserId)
-    {
+    public String addFriend(String fromUserId, String toUserId) {
         return RelationService.addFriend(fromUserId, toUserId);
     }
-    
+
     @RequestMapping("/blockNonFriend")
-    public String blockNonFriend(String fromUserId, String toUserId)
-    {
+    public String blockNonFriend(String fromUserId, String toUserId) {
         return RelationService.blockNonFriend(fromUserId, toUserId);
     }
-    
+
     @RequestMapping("/blockFriend")
-    public String blockFriend(String fromUserId, String toUserId)
-    {
+    public String blockFriend(String fromUserId, String toUserId) {
         return RelationService.blockFriend(fromUserId, toUserId);
     }
-    
+
     @RequestMapping("/approveFriend")
-    public String approveFriend(String fromUserId, String toUserId)
-    {
+    public String approveFriend(String fromUserId, String toUserId) {
         return RelationService.approveFriend(fromUserId, toUserId);
     }
-    
+
     @RequestMapping("/removeFriendRequest")
-    public String removeFriendRequest(String fromUserId, String toUserId)
-    {
+    public String removeFriendRequest(String fromUserId, String toUserId) {
         return RelationService.removeFriendRequest(fromUserId, toUserId);
     }
-    
+
     @RequestMapping("/unblockFriend")
-    public String unblockFriend(String fromUserId, String toUserId)
-    {
+    public String unblockFriend(String fromUserId, String toUserId) {
         return RelationService.unblockFriend(fromUserId, toUserId);
     }
-    
+
     @RequestMapping("/removeFriend")
-    public String removeFriend(String fromUserId, String toUserId)
-    {
+    public String removeFriend(String fromUserId, String toUserId) {
         return RelationService.removeFriend(fromUserId, toUserId);
     }
-    
+
     @RequestMapping("/getRelationList")
-    public String getRelationList(String userId, String relationTypeId, String offset, String limit)
-    {
+    public String getRelationList(String userId, String relationTypeId, String offset, String limit) {
         return RelationService.getRelationList(userId, relationTypeId, offset, limit);
     }
-    
+
     @RequestMapping("/getRelationInfo")
-    public String getRelationInfo(String fromUserId, String toUserId)
-    {
+    public String getRelationInfo(String fromUserId, String toUserId) {
         return RelationService.getRelationInfo(fromUserId, toUserId);
+    }
+
+    @RequestMapping("/getUserGenderInfo")
+    public String getUserGenderInfo(String userId) {
+        return RelationService.getUserGenderInfo(userId);
     }
 }
